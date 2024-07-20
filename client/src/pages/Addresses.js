e4e54dae1918c37aadcdea8e80c550a3e62928dd
everettthefourth <the00110100lion@gmail.com>
2024-07-20 13:20:51 -0700

    add client

---
+++ client/src/pages/Addresses.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Addresses = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4001/api/pppoe/read")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the data!", error);
+      });
+  }, []);
+
+  return (
+    <div className="Addresses pages">
+      <h1>IP Addresses</h1>
+      {data.map((item) => (
+        <div className="pppoe-info" key={item._id}>
+          <div className="important-info">
+            <p>
+              <strong>Name:</strong> {item.name}
+            </p>
+            <p>
+              <strong>IP Address: </strong>
+              <a href={`http://${item.ip}`}>{item.ip}</a>
+            </p>
+          </div>
+          {/* <div className="time-info">
+            <p>
+              <strong>Uptime:</strong> {item.uptime}
+            </p>
+            <p>
+              <strong>Timestamp:</strong> {item.timestamp}
+            </p>
+          </div> */}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Addresses;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/Home.css";
+import Addresses from "./Addresses";
+const Home = () => {
+  return (
+    <div className="Home pages">
+      <Addresses />
+    </div>
+  );
+};
+export default Home;
